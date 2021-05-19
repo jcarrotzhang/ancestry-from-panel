cc92802192fc0a1ea4d7c72429c657ef3a0484c4
Jian Carrot-Zhang <jian.carrot.zhang@gmail.com>
2021-05-19 16:21:25 -0400

    Update local_ancestry_count_blocks.py

--- GDAN_AIM/local_ancestry_count_blocks.py
+++ GDAN_AIM/local_ancestry_count_blocks.py
@@ -13,23 +13,14 @@ from scipy.stats import mannwhitneyu
 import pandas as pd
 import patsy
 
-cantype=("ACC","ESCA","LGG","PCPG","THCA","BLCA","GBM","LIHC","PRAD","THYM","BRCA","HNSC","LUAD","UCEC","CESC","KICH","LUSC","SARC","UCS","CHOL","KIRC","MESO","SKCM","UVM","COAD","KIRP","OV","STAD","DLBC","LAML","PAAD","TGCT")
+#cantype=("ACC","ESCA","LGG","PCPG","THCA","BLCA","GBM","LIHC","PRAD","THYM","BRCA","HNSC","LUAD","UCEC","CESC","KICH","LUSC","SARC","UCS","CHOL","KIRC","MESO","SKCM","UVM","COAD","KIRP","OV","STAD","DLBC","LAML","PAAD","TGCT")
 
-input2=open("TCGA_geno/gdan_aim_patient_ancestry_calls.txt")
-input3=open("/xchip/gcc_data/results2/aggregate/sif/production.postprocess.sif.txt")
+glban=open("gdan_aim_patient_ancestry_calls.txt")
 
 eur_pop={}
 afr_pop={}
 
-arrayids={}
-for line in (raw.strip().split() for raw in input3):
-       m = re.search('GenomeWideSNP_6_(.+?)$', line[3])
-       if m:
-       	arrayid=m.group(1)
-	samplename=str(line[2])
-	arrayids[samplename]=arrayid
-
-for line in (raw.strip().split() for raw in input2):
+for line in (raw.strip().split() for raw in glban):
        if "patient" not in line[0]:
                if "NA" not in line[len(line)-4]:
                        if (float(line[len(line)-4]) > 0.2) and (float(line[len(line)-4]) < 0.8) :
@@ -41,34 +32,32 @@ for line in (raw.strip().split() for raw in input2):
                                                afr_pop["TCGA-"+str(sampleID)]=line[len(line)-4]
 
 bks=[]
-OUTPUT=open("TCGA_geno/analysis/admixture_blocks.txt", 'w')
-for ctype in cantype:
-       samplelist=open("/cga/meyerson/home/zhangj/kgp/TCGA_geno/0205_seunghun/"+ctype+"/final/nofbk/samplelist")
-       files=[]
-       for line in (raw.strip().split() for raw in samplelist):
-               files.append(line[0])
-       for sample in eur_pop:
-               if sample in arrayids:
-                       sn=arrayids[sample]
-		       print "working on", ctype, sample 
-                       inputfile4 = "/cga/meyerson/home/zhangj/kgp/TCGA_geno/0205_seunghun/"+ctype+"/final/nofbk/"+sn+"_A.bed"
-                       if inputfile4 in files:
-                              input4=open(inputfile4) 
-                              for line1 in (raw.strip().split() for raw in input4):
-                                      if "UNK" not in line1[3]:
-                                                      if str(line1[0])+":"+str(line1[1]) not in bks:
-                                                          bks.append(str(line1[0])+":"+str(line1[1]))
-                                                      if str(line1[0])+":"+str(line1[2]) not in bks:
-                                                          bks.append(str(line1[0])+":"+str(line1[2]))
-                       inputfile5 = "/cga/meyerson/home/zhangj/kgp/TCGA_geno/0205_seunghun/"+ctype+"/final/nofbk/"+sn+"_B.bed"
-                       if inputfile5 in files:
-                               input5=open(inputfile5)
-                               for line1 in (raw.strip().split() for raw in input5):
-                                       if "UNK" not in line1[3]:
-						       if str(line1[0])+":"+str(line1[1]) not in bks:
-                                                                bks.append(str(line1[0])+":"+str(line1[1]))
-                                                       if str(line1[0])+":"+str(line1[2]) not in bks:
-								bks.append(str(line1[0])+":"+str(line1[2]))
+OUTPUT=open("admixture_blocks.txt", 'w')
+
+files=[]
+for line in (raw.strip().split() for raw in samplelist):
+	files.append(line[0])
+
+for sample in eur_pop:
+	print "working on", sample 
+        inputfile1 = sample+"_A.bed"
+        if inputfile1 in files:
+        	input1=open(inputfile1) 
+                for line1 in (raw.strip().split() for raw in input1):
+                           if "UNK" not in line1[3]:
+                                if str(line1[0])+":"+str(line1[1]) not in bks:
+                                        bks.append(str(line1[0])+":"+str(line1[1]))
+                                if str(line1[0])+":"+str(line1[2]) not in bks:
+                                        bks.append(str(line1[0])+":"+str(line1[2]))
+         inputfile2 = sample+"_B.bed"
+         if inputfile2 in files:
+                 input2=open(inputfile2)
+                 for line1 in (raw.strip().split() for raw in input2):
+                           if "UNK" not in line1[3]:
+				if str(line1[0])+":"+str(line1[1]) not in bks:
+                                        bks.append(str(line1[0])+":"+str(line1[1]))
+                                if str(line1[0])+":"+str(line1[2]) not in bks:
+					bks.append(str(line1[0])+":"+str(line1[2]))
                 
 for i in range(0, len(bks)):
 	print >>OUTPUT, bks[i]
